df4d553b1221c441f9056e1055bda42d8970033b
Sergey Dzyuban <dzyuban@gmail.com>
2016-01-14 08:40:12 +0000

    hosts file

---
+++ cookbooks/hostsfile/resources/mesosphere.rb
@@ -0,0 +1,39 @@
+hostsfile_entry '10.1.0.178' do
+  hostname  'sdzyuban-mesos-pc'
+  action    :create_if_missing
+end
+
+hostsfile_entry '10.1.0.166' do
+  hostname  'sdzyuban-pc'
+  action    :create_if_missing
+end
+
+hostsfile_entry '10.1.0.77' do
+  hostname  'sdzyuban-dell'
+  action    :create_if_missing
+end
+
+hostsfile_entry '10.1.0.77' do
+  hostname  'sdzyuban-ubuntu'
+  action    :create_if_missing
+end
+
+hostsfile_entry '10.1.1.49' do
+  hostname  'sdzyuban-mesos-hp'
+  action    :create_if_missing
+end
+
+hostsfile_entry '10.1.0.200' do
+  hostname  'sdzyuban-mesos-02'
+  action    :create_if_missing
+end
+
+hostsfile_entry '10.1.0.224' do
+  hostname  'sdzyuban-vm-centos'
+  action    :create_if_missing
+end
+
+hostsfile_entry '10.1.1.27' do
+  hostname 'sdzyuban-vm-2012'
+  action :create_if_missing
+end
